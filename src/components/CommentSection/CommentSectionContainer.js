0905c706c1bb838b80070a48875ed7e66fde9832
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2020-04-26 19:44:57 -0400

    posts likes and comments done need to style

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,16 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  const [comments] = useState(props.comments);
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+
+      {comments.map(comment =>
+        <Comment comment={comment} />
+        )}
+
       <CommentInput />
     </div>
   );
